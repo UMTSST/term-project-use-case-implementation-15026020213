a4b9d1f6b3ec55fdb5702028b5671f531d619fa2
15026020213 <15026020213@umt.edu.pk>
2018-05-05 17:26:57 +0500

    UseCase

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UserCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("Record.txt");
+                string Bookinfo;
+                while ((Bookinfo = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(Bookinfo);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("The file could not be opened");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
